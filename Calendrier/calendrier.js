524acb4f4769dc011c5ced702a3d7bb6af37e01b
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-10-12 18:06:05 +0200

    COntinue calendrier

--- Calendrier/calendrier.js
+++ Calendrier/calendrier.js
@@ -1,4 +1,5 @@
 function getAllDatesInMonth(month, year) {
+    month = month-1;
     let date = new Date(year, month, 1);
     let days = [];
     while (date.getMonth() === month) {
@@ -9,14 +10,72 @@ function getAllDatesInMonth(month, year) {
     return days;
 }
 
-let allDates = getAllDatesInMonth(9, 2022);
+let allDates = getAllDatesInMonth(05, 2022);
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
-    // Pour chaque date, je vais devoir créer une case dans mon tableau
+// Pour chaque date, je vais devoir créer une case dans mon tableau
 document.querySelectorAll(".calendar").forEach(calendar => {
-    allDates.forEach(date =>{
-        calendar.append(date.toLocaleDateString("fr-FR", options))
-            calendar.append(document.createElement("br"));
+    let allDatesWithWeek;
+    let firstWeekOfMonth = getWeekNumber(allDates[0]);
+
+    let table = document.createElement("table");
+    let allDatesTried = twoDimensionArray(7, 7)
+
+    allDates.forEach(date => {
+        let noSemaine = getWeekNumber(date);    
+        let DayNumber = date.getDay();
+        if(DayNumber == 0){
+            DayNumber = 7;
+        }
+        allDatesTried[(noSemaine - firstWeekOfMonth)][DayNumber] = date;
+    });
+
+    allDatesTried.forEach(semaine => {
+        let line = document.createElement("tr");
+        semaine.forEach(jour => {
+            let caseTab = document.createElement("td");
+            if((jour instanceof Date)){
+                caseTab.innerHTML = jour.getDate();
+            }
+            else{
+            }
+            line.append(caseTab)
         })
+        table.append(line);
+    })
+    calendar.append(table);
 })
 
+
+function getWeekNumber(d) {
+    // Copy date so don't modify original
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    // Set to nearest Thursday: current date + 4 - current day number
+    // Make Sunday's day number 7
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+    // Get first day of year
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+    // Calculate full weeks to nearest Thursday
+    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+    // Return array of year and week number
+    return weekNo;
+}
+
+function twoDimensionArray(a, b) {
+    let arr = [];
+
+    // creating two dimensional array
+    for (let i = 0; i< a; i++) {
+        for(let j = 0; j< b; j++) {
+            arr[i] = [];
+        }
+    }
+
+    // inserting elements to array
+    for (let i = 0; i< a; i++) {
+        for(let j = 0; j< b; j++) {
+            arr[i][j] = j;
+        }
+    }
+    return arr;
+}
